412d983bf857d85f7c2ff5cba94abcd97b7f24d4
Denin Grcic <deningrcic2@gmail.com>
2021-06-07 23:52:34 -0700

    Created set_urgency function

--- src/booking_quote_system/Package.py
+++ src/booking_quote_system/Package.py
@@ -1,4 +1,11 @@
 import uuid
+import datetime
+
+
+def set_urgency(del_date):
+    now = datetime.datetime.now()
+    urgent_date = now + datetime.timedelta(days=3)
+    return datetime.datetime.strptime(del_date, "%m-%d-%Y") < urgent_date
 
 
 class Package:
@@ -13,15 +20,12 @@ class Package:
         del_date,
     ):
         self.package_id = str(uuid.uuid4().fields[-1])[:5]
-        self.urgent = False
+        self.urgent = set_urgency(del_date)
         self.cust_fname = cust_fname
         self.cust_lname = cust_lname
         self.description = description
         self.dangerous = dangerous
         self.weight = weight
         self.volume = volume
-        self.del_date = del_date
+        self.del_date = datetime.datetime.strptime(del_date, "%m-%d-%Y")
         self.price_to_ship = 0
-
-    def set_urgency(self):
-        pass
